9a2bb9fe8e60549ae9e0e498c2a88e42133c8088
Julaam Diop <jeebe.diop@gmail.com>
2021-09-16 11:23:02 -0500

    Fix env variables and typescript errors

--- client/pages/map.tsx
+++ client/pages/map.tsx
@@ -1,162 +1,174 @@
-import type {NextPage } from 'next';
-import { GetServerSideProps } from 'next';
-import { Loader } from '@googlemaps/js-api-loader';
-import {useRef, useEffect, useState } from 'react';
-import axios from 'axios';
-import {sendQueryDetails} from './map.utils'
-import router, { useRouter } from 'next/router';
-
-
+import type { NextPage } from 'next'
+import { GetServerSideProps } from 'next'
+import { Loader } from '@googlemaps/js-api-loader'
+import { useRef, useEffect, useState } from 'react'
+import axios from 'axios'
+import { sendQueryDetails } from './map.utils'
+import router, { useRouter } from 'next/router'
 
 const Map: NextPage = () => {
-    
-    const [range, setRange] = useState(100);
-    const [magnitude, setMagnitude] = useState(3)
-    const [parameters, setParameters] = useState({range: range, magnitude: magnitude})
-    const [latLng, setLatLng] = useState({lat: 41, lng:-87})
-    const googlemap = useRef(null);
+	const [range, setRange] = useState(100)
+	const [magnitude, setMagnitude] = useState(3)
+	const [parameters, setParameters] = useState({
+		range: range,
+		magnitude: magnitude,
+	})
+	const [latLng, setLatLng] = useState({ lat: 41, lng: -87 })
+	const googlemap = useRef(null)
+	const API_KEY: any = process.env.NEXT_PUBLIC_GMAPS_API
+	useEffect(() => {
+		// const request = axios.CancelToken.source()
+		let map: google.maps.Map
 
-    useEffect(() => {
-        // const request = axios.CancelToken.source()
-        let map: google.maps.Map;
+		const loader = new Loader({
+			apiKey: API_KEY,
+			version: 'weekly',
+		})
 
-        const loader = new Loader({
-            apiKey: "AIzaSyCpIlR_C6dJUTOmhPH15sV3vDfoGP09Ztg",
-            version: "weekly",
-        });
-        
-        let markers:any = [];
-        let circles:any = [];
-        const mapOptions = {
-            center: {
-              lat: 41,
-              lng: -87
-            },
-            zoom: 4
-          };
-    
-          loader.load().then(() => {
-            map = new google.maps.Map(googlemap.current, mapOptions);
+		let markers: any = []
+		let circles: any = []
+		const mapOptions = {
+			center: {
+				lat: 41,
+				lng: -87,
+			},
+			zoom: 4,
+		}
 
-            const initMarker = new google.maps.Marker({
-                position: {
-                    lat: 41.842785,
-                    lng: -87.749486
-                },
-                map: map
-            })
-            markers.push(initMarker)
+		loader.load().then(() => {
+			map = new google.maps.Map(googlemap.current!, mapOptions);
 
-            const initCircle = new google.maps.Circle({
-                strokeColor: '#6e1710',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: "#f0aaa5",
-                fillOpacity: 0.4,
-                map,
-                center: initMarker.position,
-                radius: range * 1000
-            })
-            circles.push(initCircle)
+			const initMarker:any = new google.maps.Marker({
+				position: {
+					lat: 41.842785,
+					lng: -87.749486,
+				},
+				map: map,
+			})
+			markers.push(initMarker)
 
-            map.addListener('click', (e) => {
-                placeMarkerAndPanTo(e.latLng, map)
-                drawCircle(e.latLng)
-                sendCoordinates(e.latLng)
-            })
+			const initCircle = new google.maps.Circle({
+				strokeColor: '#6e1710',
+				strokeOpacity: 0.8,
+				strokeWeight: 2,
+				fillColor: '#f0aaa5',
+				fillOpacity: 0.4,
+				map,
+				center: initMarker.position,
+				radius: range * 1000,
+			})
+			circles.push(initCircle)
 
-            function drawCircle(center) {
-                const circle = new google.maps.Circle({
-                    strokeColor: '#6e1710',
-                    strokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: "#f0aaa5",
-                    fillOpacity: 0.4,
-                    map,
-                    center: center,
-                    radius: range * 1000
-                })
-                circles.push(circle)
-            }
-            function deleteLastMarker() {
-                markers[0].setMap(null);
-                markers = [];
-            }
+			map.addListener('click', (e) => {
+				placeMarkerAndPanTo(e.latLng, map)
+				drawCircle(e.latLng)
+				sendCoordinates(e.latLng)
+			})
 
-            function deleteLastCircle() {
-                circles[0].setMap(null);
-                circles = [];
-            }
+			function drawCircle(center:any) {
+				const circle = new google.maps.Circle({
+					strokeColor: '#6e1710',
+					strokeOpacity: 0.8,
+					strokeWeight: 2,
+					fillColor: '#f0aaa5',
+					fillOpacity: 0.4,
+					map,
+					center: center,
+					radius: range * 1000,
+				})
+				circles.push(circle)
+			}
+			function deleteLastMarker() {
+				markers[0].setMap(null)
+				markers = []
+			}
 
-            function placeMarkerAndPanTo(latLng: google.maps.LatLng, map: google.maps.Map) {
-                deleteLastMarker();
-                deleteLastCircle();
-                const newMarker = new google.maps.Marker({
-                  position: latLng,
-                  map: map,
-                });
-                markers.push(newMarker);
-                map.panTo(latLng);
-            }
+			function deleteLastCircle() {
+				circles[0].setMap(null)
+				circles = []
+			}
 
-            function sendCoordinates(latLng:google.maps.LatLng) {
-                axios.post('http://localhost:3005/map',  {
-                    position: latLng
-                })
-                .then((res) => {
-                    console.log(res)
-                })
-            }
-            
-        });
+			function placeMarkerAndPanTo(
+				latLng: google.maps.LatLng,
+				map: google.maps.Map
+			) {
+				deleteLastMarker()
+				deleteLastCircle()
+				const newMarker = new google.maps.Marker({
+					position: latLng,
+					map: map,
+				})
+				markers.push(newMarker)
+				map.panTo(latLng)
+			}
 
-    }, [parameters]);
+			function sendCoordinates(latLng: google.maps.LatLng) {
+				axios
+					.post('http://localhost:3005/map', {
+						position: latLng,
+					})
+					.then((res) => {
+						console.log(res)
+					})
+			}
+		})
+	}, [parameters])
 
-    function getEarthquakes() {
-        axios.get('http://localhost:3005/earthquakes')
-        .then((res) => {
-            console.log(res)
-        })
-    }
+	function getEarthquakes() {
+		axios.get('http://localhost:3005/earthquakes').then((res) => {
+			console.log(res)
+		})
+	}
 
-    return (
-        <div className="flex flex-col items-center bg-">
-            <h1 className="items-center text-xl font-bold justify-center p-4">Earthquake Locator</h1>
-            <div className="h-96 w-full rounded-2xl border-4 border-solid border-black" ref={googlemap}></div>
+	return (
+		<div className='flex flex-col items-center bg-'>
+			<h1 className='items-center text-xl font-bold justify-center p-4'>
+				Earthquake Locator
+			</h1>
+			<div
+				className='h-96 w-full rounded-2xl border-4 border-solid border-black'
+				ref={googlemap}></div>
 
-            <div className="flex flex-col bg-pink-300 text-black w-3/4 p-1 my-5 text-xl font-semibold">
-                <h1 className=""> Choose range(in km) of earthquake</h1>
-                <input type="number" className="text-black" onChange={(e) => {
-                        setRange(Number(e.target.value))}
-                    } />
+			<div className='flex flex-col bg-pink-300 text-black w-3/4 p-1 my-5 text-xl font-semibold'>
+				<h1 className=''> Choose range(in km) of earthquake</h1>
+				<input
+					type='number'
+					className='text-black'
+					onChange={(e) => {
+						setRange(Number(e.target.value))
+					}}
+				/>
 
-                <h1 className=""> Choose minimum magnitude of earthquake</h1>
-                <input type="number" className="text-black" onChange={(e) => {
-                    setMagnitude(Number(e.target.value))}
-                    }/>
-            </div>
-            <button className="w-auto place-content-center my-5 px-10 py-3 bg-green-900 uppercase text-white font-semibold shadow-md rounded-md transiiton duration-200 ease-in-out hover:bg-green-600"
-                onClick={() => {
-                    setParameters({range, magnitude});
-                }}
-            >Set Parameters</button>
-            <button className="w-auto place-content-center my-5 px-10 py-3 bg-green-900 uppercase text-white font-semibold shadow-md rounded-md transiiton duration-200 ease-in-out hover:bg-green-600"
-                onClick={() => {
-                    sendQueryDetails(range, magnitude);
-                    getEarthquakes()
-                    setLatLng(latLng);
-                    router.push('/results')
-                }}> 
-                Search 
-            </button>
+				<h1 className=''> Choose minimum magnitude of earthquake</h1>
+				<input
+					type='number'
+					className='text-black'
+					onChange={(e) => {
+						setMagnitude(Number(e.target.value))
+					}}
+				/>
+			</div>
+			<button
+				className='w-auto place-content-center my-5 px-10 py-3 bg-green-900 uppercase text-white font-semibold shadow-md rounded-md transiiton duration-200 ease-in-out hover:bg-green-600'
+				onClick={() => {
+					setParameters({ range, magnitude })
+				}}>
+				Set Parameters
+			</button>
+			<button
+				className='w-auto place-content-center my-5 px-10 py-3 bg-green-900 uppercase text-white font-semibold shadow-md rounded-md transiiton duration-200 ease-in-out hover:bg-green-600'
+				onClick={() => {
+					sendQueryDetails(range, magnitude)
+					getEarthquakes()
+					setLatLng(latLng)
+					router.push('/results')
+				}}>
+				Search
+			</button>
 
-            {
-
-            }
-        </div>
-        
-        
-    )
+			{}
+		</div>
+	)
 }
 
 export default Map
--- client/pages/map.utils.tsx
+++ client/pages/map.utils.tsx
@@ -1,128 +1,129 @@
-import axios from "axios"
-import { Loader } from '@googlemaps/js-api-loader';
+import axios from 'axios'
+import { Loader } from '@googlemaps/js-api-loader'
 
-interface Coordinates { 
-    position: {
-        lat: Number,
-        lng: Number
-    }
+interface Coordinates {
+	position: {
+		lat: Number
+		lng: Number
+	}
 }
 
 interface Marker {
-    position: {
-        lat: Number,
-        lng: Number
-    }
+	position: {
+		lat: Number
+		lng: Number
+	}
 }
 
 interface Circle {
-    strokeColor: string,
-    strokeOpacity: number,
-    strokeWeight: number,
-    fillColor: string,
-    fillOpacity: number,
-    map: google.map.Maps,
-    center: number,
-    radius: number
-
+	strokeColor: string
+	strokeOpacity: number
+	strokeWeight: number
+	fillColor: string
+	fillOpacity: number
+	map: google.map.Maps
+	center: number
+	radius: number
 }
-
+const API_KEY: any = process.env.NEXT_PUBLIC_GMAPS_API
 const loader = new Loader({
-    apiKey: "AIzaSyCpIlR_C6dJUTOmhPH15sV3vDfoGP09Ztg",
-    version: "weekly",
-});
+	apiKey: API_KEY,
+	version: 'weekly',
+})
 
-let map: google.maps.Map;
-let markers:Marker[] = [];
-let circles:Circle[] = [];
+let map: google.maps.Map
+let markers: Marker[] = []
+let circles: Circle[] = []
 
 function sendQueryDetails(range: number, magnitude: number) {
-    const QUERYENDPOINT = 'http://localhost:3005/map/query'
-    axios.post(QUERYENDPOINT, {
-        range: range,
-        magnitude: magnitude
-    })
-    .then((res) => {
-        console.log(res)
-    })
+	const QUERYENDPOINT = 'http://localhost:3005/map/query'
+	axios
+		.post(QUERYENDPOINT, {
+			range: range,
+			magnitude: magnitude,
+		})
+		.then((res) => {
+			console.log(res)
+		})
 }
 
-function drawCircle(map: google.maps.Map, center:Coordinates | null, range:number) {
-    const circle = new google.maps.Circle({
-        strokeColor: '#6e1710',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#f0aaa5",
-        fillOpacity: 0.4,
-        map,
-        center: center,
-        radius: range * 1000
-    })
+function drawCircle(
+	map: google.maps.Map,
+	center: Coordinates | null,
+	range: number
+) {
+	const circle = new google.maps.Circle({
+		strokeColor: '#6e1710',
+		strokeOpacity: 0.8,
+		strokeWeight: 2,
+		fillColor: '#f0aaa5',
+		fillOpacity: 0.4,
+		map,
+		center: center,
+		radius: range * 1000,
+	})
 }
 
-function loadGoogleMaps(markers: Marker[],circles: Circle[], range:number) {
-        map = new google.maps.Map(googlemap.current, mapOptions);
+function loadGoogleMaps(markers: Marker[], circles: Circle[], range: number) {
+	map = new google.maps.Map(googlemap.current, mapOptions)
 
-        const initMarker = new google.maps.Marker({
-            position: {
-                lat: 41.842785,
-                lng: -87.749486
-            },
-            map: map
-        })
-        markers.push(initMarker)
+	const initMarker = new google.maps.Marker({
+		position: {
+			lat: 41.842785,
+			lng: -87.749486,
+		},
+		map: map,
+	})
+	markers.push(initMarker)
 
-        const initCircle = new google.maps.Circle({
-            strokeColor: '#6e1710',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: "#f0aaa5",
-            fillOpacity: 0.4,
-            map,
-            center: initMarker.position,
-            radius: range * 1000
-        })
-        circles.push(initCircle)
+	const initCircle = new google.maps.Circle({
+		strokeColor: '#6e1710',
+		strokeOpacity: 0.8,
+		strokeWeight: 2,
+		fillColor: '#f0aaa5',
+		fillOpacity: 0.4,
+		map,
+		center: initMarker.position,
+		radius: range * 1000,
+	})
+	circles.push(initCircle)
 
-        map.addListener('click', (e) => {
-            placeMarkerAndPanTo(e.latLng, map)
-            drawCircle(e.latLng)
-            sendCoordinates(e.latLng)
-        })
+	map.addListener('click', (e) => {
+		placeMarkerAndPanTo(e.latLng, map)
+		drawCircle(e.latLng)
+		sendCoordinates(e.latLng)
+	})
 }
 
 function deleteLastMarker() {
-    markers[0].setMap(null);
-    markers = [];
+	markers[0].setMap(null)
+	markers = []
 }
 
 function deleteLastCircle() {
-    circles[0].setMap(null);
-    circles = [];
+	circles[0].setMap(null)
+	circles = []
 }
 
 function placeMarkerAndPanTo(latLng: google.maps.LatLng, map: google.maps.Map) {
-    deleteLastMarker();
-    deleteLastCircle();
-    const newMarker = new google.maps.Marker({
-      position: latLng,
-      map: map,
-    });
-    markers.push(newMarker);
-    map.panTo(latLng);
+	deleteLastMarker()
+	deleteLastCircle()
+	const newMarker = new google.maps.Marker({
+		position: latLng,
+		map: map,
+	})
+	markers.push(newMarker)
+	map.panTo(latLng)
 }
 
-function sendCoordinates(latLng:google.maps.LatLng) {
-    axios.post('http://localhost:3005/map',  {
-        position: latLng
-    })
-    .then((res) => {
-        console.log(res)
-    })
+function sendCoordinates(latLng: google.maps.LatLng) {
+	axios
+		.post('http://localhost:3005/map', {
+			position: latLng,
+		})
+		.then((res) => {
+			console.log(res)
+		})
 }
 
-export {
-    sendQueryDetails,
-    drawCircle,
-    loader
-}
+export { sendQueryDetails, drawCircle, loader }
